46eb4d28f703156f0923dcda9cbef68fbc5d008e
Marco Túlio <marcotmotta@gmail.com>
2022-03-21 23:31:48 -0300

    home component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 
 import Login from './components/login/login';
+import Home from './components/home/home';
 
 function App() {
   return (
     <div className="App">
       <Login/>
+      <Home/>
     </div>
   );
 }
---
+++ src/components/home/home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './home.scss'
+
+export default function home() {
+  return (
+    <div class="home">
+        Welcome, you're logged in!
+    </div>
+  )
+}
